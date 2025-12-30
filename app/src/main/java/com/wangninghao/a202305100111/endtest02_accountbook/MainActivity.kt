6720644455e6c85b1f7cb894cb507fd10d3a4ea6
86159 <2@>
2025-12-30 16:44:43 +0800

    UI开发登陆、记账

--- app/src/main/java/com/wangninghao/a202305100111/endtest02_accountbook/MainActivity.kt
+++ app/src/main/java/com/wangninghao/a202305100111/endtest02_accountbook/MainActivity.kt
@@ -5,16 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.wangninghao.a202305100111.endtest02_accountbook.databinding.ActivityMainBinding
 
+/**
+ * 主界面
+ * 包含底部导航栏和Fragment容器
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 处理系统栏
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            // 底部导航栏处理底部padding
+            binding.bottomNavigation.setPadding(0, 0, 0, systemBars.bottom)
             insets
         }
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // 将底部导航与NavController关联
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
